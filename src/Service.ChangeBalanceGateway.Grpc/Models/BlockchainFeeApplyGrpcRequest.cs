f4a4e8a2503e9b0fee45e57a5d5ee707abbc468d
Alexey Novichikhin <novichikhin.au@gmail.com>
2021-03-24 15:57:27 +0200

    add brokerId to BlockchainFeeApplyGrpcRequest

--- src/Service.ChangeBalanceGateway.Grpc/Models/BlockchainFeeApplyGrpcRequest.cs
+++ src/Service.ChangeBalanceGateway.Grpc/Models/BlockchainFeeApplyGrpcRequest.cs
@@ -7,7 +7,8 @@ namespace Service.ChangeBalanceGateway.Grpc.Models
     {
         [DataMember(Order = 1)] public string TransactionId { get; set; }
         [DataMember(Order = 2)] public string WalletId { get; set; }
-        [DataMember(Order = 3)] public string AssetSymbol { get; set; }
-        [DataMember(Order = 3)] public double FeeAmount { get; set; }
+        [DataMember(Order = 3)] public string BrokerId { get; set; }
+        [DataMember(Order = 4)] public string AssetSymbol { get; set; }
+        [DataMember(Order = 5)] public double FeeAmount { get; set; }
     }
 }
